ad684255da7c3ba46ceb0bc367c9fd0cac870de5
AIIS188 <1160226068@qq.com>
2024-10-24 18:55:26 +0800

    修改1，写2

--- exp1/Vertor.h
+++ exp1/Vertor.h
@@ -250,6 +250,7 @@ Rank Vector<T>:: search(T const& e,Rank lo ,Rank hi )const{
 template<typename T>
 T& Vector<T>::operator[](Rank r ) const{
     return _elem[r];
+    
 }
 
 //向量模板类赋值符的重载
@@ -279,18 +280,11 @@ int Vector<T>::remove(Rank lo ,Rank hi){
 
 template<typename T>
 Rank Vector<T>::insert(Rank r , T const& e){
-    
-    Vector<T> A(_elem,_size);
-    _elem=new T[_capacity<<=1];_size=0;
-    Rank i=0;
-    while(i<A.size()){
-        if(_size!=r){
-        _elem[_size++]=A[i++];
-        }
-        else{_elem[_size++]=e;}
-    }
-    
-    return r;
+    expand();
+    for(int i  = _size ;  i > r ; i-- )_elem[i]=_elem[i-1];
+    _elem[r] = e ; 
+    _size++;
+    return r ;
 }
 
 
@@ -420,83 +414,3 @@ Vector<T> Vector<T>::findmodel(double m1,double m2,Rank lo,Rank hi){
     return A;
 }
 
-int main(){
-    //（1）
-    Vector<Complex> A(10000);
-    for(int i;i<10000;i++){
-        int j=rand()%100*rand()%100,k=rand()%100*rand()%100;
-        A[i].reset(j,k);  
-    }
-    A.setsize(10000);
-    Complex B(10,10);
-    //插入
-    A.insert(3,B);
-    A.insert(4,B);
-    A.insert(5,B);
-    //无序去重
-    A.deduplicate();
-    A.msort();
-    //置乱
-    // A.unsort();
-    // A.traverse(allprint1);
-    // cout<<"\n"<<A.find(B)<<"\n";
-    //删除
-    // A.remove(0);
-    // A.traverse(allprint1);
-    //无序去重
-    // A.deduplicate();
-    // 有序去重
-    // A.uniquify();
-    
-    // A.traverse(allprint1);
-    //（2）
-    Vector<Complex> C(A),D(A);//逆序，乱序向量生成
-    C.resort();
-    D.unsort();
-//时间对象声明
-    clock_t start_time,end_time;
-    double total_time;
-//顺序排序时间
-    Vector<Complex> A1(A),A2(A);
-    start_time=clock();
-    A1.sort();//冒泡排序
-    end_time=clock();
-    total_time= double(end_time - start_time) / CLOCKS_PER_SEC;
-    cout<<"冒泡顺序排序所用时间："<<total_time<<"\n";
-
-    start_time=clock();
-    A2.msort();
-    end_time=clock();
-    total_time= double(end_time - start_time) / CLOCKS_PER_SEC;
-    cout<<"归并顺序排序所用时间："<<total_time<<"\n";
-//乱序排序时间
-    Vector<Complex> D1(D),D2(D);
-    start_time=clock();
-    D1.sort();
-    end_time=clock();
-    total_time= double(end_time - start_time) / CLOCKS_PER_SEC;
-    cout<<"冒泡乱序排序所用时间："<<total_time<<"\n";
-
-    start_time=clock();
-    D2.msort();
-    end_time=clock();
-    total_time= double(end_time - start_time) / CLOCKS_PER_SEC;
-    cout<<"归并乱序排序所用时间："<<total_time<<"\n";
-//逆序排序时间
-    Vector<Complex> C1(C),C2(C);
-    start_time=clock();
-    C1.sort();
-    end_time=clock();
-    total_time= double(end_time - start_time) / CLOCKS_PER_SEC;
-    cout<<"冒泡逆序排序时间：" <<total_time<<"\n";
-
-    start_time=clock();
-    C2.msort();
-    end_time=clock();
-    total_time= double(end_time - start_time) / CLOCKS_PER_SEC;
-    cout<<"归并逆序排序时间："<<total_time<<"\n";
-//(3)返回子向量输出验证
-    Vector<Complex> search_A;
-    search_A=A.findmodel(100,120);
-    search_A.traverse(allprint1);
-};
---
+++ exp1/main.cpp
@@ -0,0 +1,90 @@
+#include<iostream>
+#include<math.h>
+#include<random>
+#include<ctime>
+#include "D:\数据结构\DS24\exp1\Vertor.h"
+
+
+
+
+
+int main(){
+    //（1）
+    Vector<Complex> A(10000);
+    for(int i;i<10000;i++){
+        int j=rand()%100*rand()%100,k=rand()%100*rand()%100;
+        A[i].reset(j,k);  
+    }
+    A.setsize(10000);
+    Complex B(10,10);
+    //插入
+    A.insert(3,B);
+    A.insert(4,B);
+    A.insert(5,B);
+    //无序去重
+    A.deduplicate();
+    A.msort();
+    //置乱
+    // A.unsort();
+    // A.traverse(allprint1);
+    // cout<<"\n"<<A.find(B)<<"\n";
+    //删除
+    // A.remove(0);
+    // A.traverse(allprint1);
+    //无序去重
+    // A.deduplicate();
+    // 有序去重
+    // A.uniquify();
+    
+    // A.traverse(allprint1);
+    //（2）
+    Vector<Complex> C(A),D(A);//逆序，乱序向量生成
+    C.resort();
+    D.unsort();
+//时间对象声明
+    clock_t start_time,end_time;
+    double total_time;
+//顺序排序时间
+    Vector<Complex> A1(A),A2(A);
+    start_time=clock();
+    A1.sort();//冒泡排序
+    end_time=clock();
+    total_time= double(end_time - start_time) / CLOCKS_PER_SEC;
+    cout<<"冒泡顺序排序所用时间："<<total_time<<"\n";
+
+    start_time=clock();
+    A2.msort();
+    end_time=clock();
+    total_time= double(end_time - start_time) / CLOCKS_PER_SEC;
+    cout<<"归并顺序排序所用时间："<<total_time<<"\n";
+//乱序排序时间
+    Vector<Complex> D1(D),D2(D);
+    start_time=clock();
+    D1.sort();
+    end_time=clock();
+    total_time= double(end_time - start_time) / CLOCKS_PER_SEC;
+    cout<<"冒泡乱序排序所用时间："<<total_time<<"\n";
+
+    start_time=clock();
+    D2.msort();
+    end_time=clock();
+    total_time= double(end_time - start_time) / CLOCKS_PER_SEC;
+    cout<<"归并乱序排序所用时间："<<total_time<<"\n";
+//逆序排序时间
+    Vector<Complex> C1(C),C2(C);
+    start_time=clock();
+    C1.sort();
+    end_time=clock();
+    total_time= double(end_time - start_time) / CLOCKS_PER_SEC;
+    cout<<"冒泡逆序排序时间：" <<total_time<<"\n";
+
+    start_time=clock();
+    C2.msort();
+    end_time=clock();
+    total_time= double(end_time - start_time) / CLOCKS_PER_SEC;
+    cout<<"归并逆序排序时间："<<total_time<<"\n";
+//(3)返回子向量输出验证
+    Vector<Complex> search_A;
+    search_A=A.findmodel(100,120);
+    search_A.traverse(allprint1);
+};
